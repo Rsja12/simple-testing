6b519f4dc91a6b2057b5abeb51a745d5924254c7
Rsja12 <asocarras10@gmail.com>
2020-08-06 19:29:57 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const googleDataBase = [
+    'cats.com',
+    'souprecipes.com',
+    'flowers.com',
+    'animals.com',
+    'catpictures.com',
+    'myfavoritecats.com'
+];
+
+const googleSearch = searchInputs => {
+    
+}
